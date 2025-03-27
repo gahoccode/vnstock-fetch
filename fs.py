479b118df4f0850ca64d7031466b98942f442175
Your Name <your.email@example.com>
2025-03-27 22:47:20 +0700

    Add More Company Data

--- fs.py
+++ fs.py
@@ -16,5 +16,54 @@ def save_to_csv():
     IncomeStatement.to_csv('IncomeStatement.csv', index=False)
     CashFlow.to_csv('CashFlow.csv', index=False)
     Ratio.to_csv('Ratio.csv', index=False)
-save_to_csv()
+#save_to_csv()
 
+company = Vnstock().stock(symbol='VCB', source='TCBS').company
+
+"""#### Tổng quan"""
+
+CompanyOverview = company.overview()
+
+"""#### Hồ sơ"""
+
+CompanyProfile = company.profile()
+
+"""#### Cổ đông"""
+
+CompanyShareholders = company.shareholders()
+
+"""#### Giao dịch nội bộ"""
+
+CompanyInsiderDeals = company.insider_deals().head(3)
+
+"""#### Công ty con, liên kết"""
+
+CompanySubsidiaries = company.subsidiaries().head()
+
+"""#### Ban lãnh đạo"""
+
+CompanyOfficers = company.officers().head()
+
+"""#### Sự kiện"""
+
+CompanyEvents = company.events().head()
+
+"""#### Tin tức"""
+
+CompanyNews = company.news().head()
+
+"""#### Cổ tức"""
+
+CompanyDividends = company.dividends().head()
+
+def save_company_data():
+    CompanyOverview.to_csv('CompanyOverview.csv', index=False)
+    CompanyProfile.to_csv('CompanyProfile.csv', index=False)
+    CompanyShareholders.to_csv('CompanyShareholders.csv', index=False)
+    CompanyInsiderDeals.to_csv('CompanyInsiderDeals.csv', index=False)
+    CompanySubsidiaries.to_csv('CompanySubsidiaries.csv', index=False)
+    CompanyOfficers.to_csv('CompanyOfficers.csv', index=False)
+    CompanyEvents.to_csv('CompanyEvents.csv', index=False)
+    CompanyNews.to_csv('CompanyNews.csv', index=False)
+    CompanyDividends.to_csv('CompanyDividends.csv', index=False)
+#save_company_data()
